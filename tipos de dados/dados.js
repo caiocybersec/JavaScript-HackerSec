03b8a70ad58a27eb340518e1623e6315bc603793
caio alves esl <dacruzcaioalves@gmail.com>
2024-06-06 10:26:11 -0300

    Tipos de dados

---
+++ tipos de dados/dados.js
@@ -0,0 +1,10 @@
+const string = ""
+const number = 10
+const boolean = true
+const object = {
+    titulo: "HackerSec"
+}
+
+const array = [{fruta: "banana"}, {fruta: "Maça" }]
+
+console.log(typeof number)
